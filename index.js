13e047b3936f8b317b42515bfe37a0591c4515a2
Dipokal HHJ <48173908+DipokalLab@users.noreply.github.com>
2020-04-17 00:33:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ app.get('/', function(req, res) {
     res.send("Hello World");
 });
 
-server.listen(4000);
+server.listen(3001, function() {
+  console.log('EBS: >> Server listen on port ' + server.address().port);
+  console.log('EBS: Start >> EBS 프로젝트에 오신것을 환영합니다.');
+});
+
